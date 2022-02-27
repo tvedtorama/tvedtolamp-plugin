2846004809a04fcfcfcd201b1c0769f2c7628a9a
Jørgen Tvedt <tvedtorama@gmail.com>
2022-02-27 18:00:52 +0100

    Wrong mod value

--- src/writer.ts
+++ src/writer.ts
@@ -1,4 +1,4 @@
 import {default as Axios} from "axios"
 
 export const write = (value: number) => 
-	Axios.get(`http://192.168.68.251/analog/6/${value % 255}`)
+	Axios.get(`http://192.168.68.251/analog/6/${value % 256}`)
